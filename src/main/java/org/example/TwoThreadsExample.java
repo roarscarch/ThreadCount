f51a9f72861ec4264bffa12c99c725e676228fa8
Anurag <111561835+roarscarch@users.noreply.github.com>
2023-12-22 02:24:43 +0530

    Update TwoThreadsExample.java

--- src/main/java/org/example/TwoThreadsExample.java
+++ src/main/java/org/example/TwoThreadsExample.java
@@ -2,21 +2,21 @@ package org.example;
 
 public class TwoThreadsExample {
 
-    // Shared flag to signal the other thread to stop
+  
     private static volatile boolean shouldStop = false;
 
     public static void main(String[] args) {
-        // Create and start the first thread
+       
         Thread thread1 = new Thread(new CountingTask());
 
-        // Create and start the second thread
+       
         Thread thread2 = new Thread(new PrintingTask());
 
-        // Start both threads
+      
         thread1.start();
         thread2.start();
 
-        // Wait for the main thread to complete
+       
         try {
             thread1.join();
             thread2.join();
@@ -27,18 +27,18 @@ public class TwoThreadsExample {
         System.out.println("Main thread: All threads completed");
     }
 
-    // Task for the first thread
+  
     static class CountingTask implements Runnable {
         @Override
         public void run() {
             for (int i = 1; i <= 10 && !shouldStop; i++) {
                 System.out.println("Thread 1: Counting " + i);
             }
-            shouldStop = true; // Signal the second thread to stop
+            shouldStop = true;
         }
     }
 
-    // Task for the second thread
+  
     static class PrintingTask implements Runnable {
         @Override
         public void run() {
@@ -47,7 +47,7 @@ public class TwoThreadsExample {
                 System.out.println("Thread 2: Printing " + c);
                 c++;
             }
-            shouldStop = true; // Signal the first thread to stop
+            shouldStop = true;
         }
     }
 }
